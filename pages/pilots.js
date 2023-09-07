fbd82419bc19c1fb6ad57be00bf46ca147ab5a9b
NiyazImamov <125542331+NikoImam@users.noreply.github.com>
2023-09-08 00:45:30 +0300

    Upload website

---
+++ pages/pilots.js
@@ -0,0 +1,47 @@
+async function viewPilots() {
+    let response = await fetch('http://ergast.com/api/f1/drivers.json?limit=1000');
+
+    if (response.ok) {
+        let json = await response.json();
+        let drivers = json.MRData.DriverTable.Drivers;
+
+        const list = document.getElementById("pilots-list");
+
+        for (let i = 0; i < drivers.length; i++) {
+            const pilotInfo = document.createElement("li");
+            pilotInfo.setAttribute("class", "pilotInfo");
+
+            const pilotInfoList = document.createElement("ul");
+            pilotInfoList.setAttribute("class", "pilotInfoList");
+
+            let name = document.createElement("li");
+            name.innerHTML = drivers[i].givenName;
+            name.setAttribute("class", "pilotLi")
+
+
+            let familyName = document.createElement("li");
+            familyName.innerHTML = drivers[i].familyName;
+            familyName.setAttribute("class", "pilotLi");
+
+            let dateOfBirth = document.createElement("li");
+            dateOfBirth.innerHTML = drivers[i].dateOfBirth;
+            dateOfBirth.setAttribute("class", "pilotLi");
+
+            let wiki = document.createElement("li");
+            let wikiRef = document.createElement("a");
+            wikiRef.href = drivers[i].url;
+            wikiRef.innerHTML = "wiki";
+            wiki.setAttribute("class", "pilotLi");
+            wiki.appendChild(wikiRef);
+
+            pilotInfoList.appendChild(name);
+            pilotInfoList.appendChild(familyName);
+            pilotInfoList.appendChild(dateOfBirth);
+            pilotInfoList.appendChild(wiki);
+            pilotInfo.appendChild(pilotInfoList);
+            list.appendChild(pilotInfo);
+        }
+    } else {
+        alert("Ошибка: " + response.status);
+    }
+}
---
+++ pages/seasonsInfo.js
@@ -0,0 +1,62 @@
+async function getAllSeasonsList() {
+    let response = await fetch('http://ergast.com/api/f1/seasons.json?limit=100');
+    if (response.ok) {
+        let json = await response.json();
+        let seasons = json.MRData.SeasonTable.Seasons;
+        const selectList = document.getElementById("season-select");
+        for (let i = 0; i < seasons.length; i++) {
+            let option = document.createElement("option");
+            option.innerHTML = seasons[i].season;
+            option.value = seasons[i].season;
+            selectList.appendChild(option);
+        }
+    } else {
+        alert("Ошибка: " + response.status);
+    }
+}
+
+async function getAllRoundsList() {
+    let selectedSeason = document.querySelector('#season-select');
+    let year = selectedSeason.value;
+    let response = await fetch('http://ergast.com/api/f1/' + year + '.json?limit=50');
+    if (response.ok) {
+        let json = await response.json();
+        let races = json.MRData.RaceTable.Races;
+        const selectList = document.getElementById("round-select");
+        selectList.innerHTML = '<option value="" selected disabled>Choose round</option>';
+        for (let i = 0; i < races.length; i++) {
+            let option = document.createElement("option");
+            option.innerHTML = races[i].round;
+            option.value = races[i].round;
+            selectList.appendChild(option);
+        }
+    } else {
+        alert("Ошибка: " + response.status);
+    }
+}
+
+async function getSeasonInfo() {
+    let selectedSeason = document.querySelector('#season-select');
+    let year = selectedSeason.value;
+    let selectedRound = document.querySelector('#round-select');
+    let round = selectedRound.value;
+    let response = await fetch('http://ergast.com/api/f1/' + year + '/' + round + '/results.json');
+    if (response.ok) {
+        let json = await response.json();
+        let info = json.MRData.RaceTable.Races[0];
+        document.getElementById("info").innerHTML =
+            "<p>Season: " + year + '</p>' +
+            "<p>Round: " + round + '</p>' +
+            "<p>Race: " + info.raceName + '</p>' +
+            "<p>Circuit: " + info.Circuit.circuitName + '</p>' +
+            "<p>Date: " + info.date + '</p>' +
+            "<p>Winner: " + info.Results[0].Driver.givenName + ' ' + info.Results[0].Driver.familyName + '</p>' +
+            "<p>Number: " + info.Results[0].number + '</p>' +
+            "<p>Grid: " + info.Results[0].grid + '</p>' +
+            "<p>Constructor: " + info.Results[0].Constructor.name + '</p>' +
+            "<p>Time: " + info.Results[0].Time.time + '</p>';
+        document.getElementById("info").style.border = "2px solid rgba(111, 106, 172, 0.788)";
+    } else {
+        alert("Ошибка: " + response.status);
+    }
+}
