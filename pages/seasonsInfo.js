1003bb0ae8201e530e07b9ad18fba7712e9f1ab9
NiyazImamov <125542331+NikoImam@users.noreply.github.com>
2023-09-09 20:45:22 +0300

    Update seasonsInfo.js

--- pages/seasonsInfo.js
+++ pages/seasonsInfo.js
@@ -1,5 +1,5 @@
 async function getAllSeasonsList() {
-    let response = await fetch('http://ergast.com/api/f1/seasons.json?limit=30');
+    let response = await fetch('http://ergast.com/api/f1/seasons.json?limit=100');
     if (response.ok) {
         let json = await response.json();
         let seasons = json.MRData.SeasonTable.Seasons;
