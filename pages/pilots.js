1b0db04a8aa5aa69db3c31aa0650a6843c4a1c38
NiyazImamov <125542331+NikoImam@users.noreply.github.com>
2023-09-09 20:48:55 +0300

    Update pilots.js

--- pages/pilots.js
+++ pages/pilots.js
@@ -1,5 +1,5 @@
 async function viewPilots() {
-    let response = await fetch('http://ergast.com/api/f1/drivers.json?limit=900');
+    let response = await fetch('http://ergast.com/api/f1/drivers.json?limit=30');
 
     if (response.ok) {
         let json = await response.json();
