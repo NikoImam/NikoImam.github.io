87b7a0a1810988f19474fa9992bae7d2786180da
NikoImam <niyaz120904@gmail.com>
2023-10-15 16:12:18 +0300

    Beginnig of probTheory project

---
+++ probTheory/viewer.js
@@ -0,0 +1,12 @@
+async function viewBlocks() {
+    await viewHeader();
+}
+
+async function viewHeader() {
+    header = document.getElementById("header");
+    header.innerHTML = '<p>h</p>';
+}
+
+async function viewFooter() {
+
+}
